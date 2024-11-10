b564537ed68c47a4ae2b2c8f5cfd72e9d5daae91
Shi-bal <134521460+Shi-bal@users.noreply.github.com>
2024-11-10 17:41:35 +0800

    Delete rmi-enrollment-20241002T060142Z-001/rmi-enrollment directory

--- rmi-enrollment-20241002T060142Z-001/rmi-enrollment/server/CourseInterface.java
+++
@@ -1,7 +0,0 @@
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface CourseInterface extends Remote {
-    String getId() throws RemoteException;
-    String getName() throws RemoteException;
-}
